d715344aab8d7c64df3dcc5d64c31e3de0c0bd8c
Matt Hackmann <matt@dxprog.com>
2016-04-26 21:16:33 -0700

    profiles scraped to JSON files

--- config.example.js
+++ config.example.js
@@ -1,3 +1,5 @@
 module.exports = {
-  cookie: 'YOUR_OKC_COOKIE'
+  cookie: 'YOUR_OKC_COOKIE',
+  seed: 'SOME_OKC_PROFILE_NAME',
+  maxProfiles: 100
 };
--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+
+const config = require('./config');
 const scrapeProfile = require('./profile');
 
-scrapeProfile('SOME_USER_HERE').then((profile) => {
-  console.log(profile);
-});
+const scrapeQueue = [];
+const scanned = [];
+
+let count = 0;
+
+function scrapeNextProfile() {
+  const username = scrapeQueue.shift();
+  if (username || count > config.maxProfiles) {
+    scrapeProfile(username).then((profile) => {
+      console.log(`Profile "${username}" saved.`);
+      fs.writeFileSync(path.resolve('profiles', `${username}.json`), JSON.stringify(profile));
+
+      profile.similar.forEach((item) => {
+        if (scanned.indexOf(item) === -1) {
+          scrapeQueue.push(item);
+        }
+      });
+
+      scanned.push(username);
+      count++;
+    }).then(scrapeNextProfile);
+  } else {
+    console.log(`${count} profiles scraped`);
+  }
+}
+
+scrapeQueue.push(config.seed);
+scrapeNextProfile();
